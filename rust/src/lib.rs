a640bb1c4f8051cc2ff21b45d015a2f160463b39
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2020-10-03 02:49:37 -0400

    Setup for use with jq

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,8 @@
 mod types;
 use anyhow::Error;
 /// term -> response
+/// maybe just need webenv?
+/// ie: https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&query_key=1&usehistory=y&WebEnv=MCID_5f77bdfcb48029616024b3fb&retmode=xml&retmax=100&retstart=18
 /// response -> xml
 /// xml -> id's
 /// ids -> responses
@@ -10,9 +12,9 @@ use types::*;
 
 const BASE_URL: &str = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/";
 
-pub async fn pubmed_query(query: &str) -> Result<Response, Error> {
+pub async fn pubmed_query(query: &str, retmode: &str) -> Result<Response, Error> {
     let client = Client::new();
-    let query = [("db", "pubmed"), ("term", query)];
+    let query = [("db", "pubmed"), ("term", query), ("retmode", retmode)];
     let base_url = BASE_URL.to_string() + "esearch.fcgi";
 
     client
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,12 +3,12 @@ use anyhow::Error;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let term = "breast+cancer";
-    let response = rust::pubmed_query(term).await?;
-    println!("Status: {}, URL: {}", response.status(), response.url());
-    println!("Headers:\n{:#?}", response.headers());
+    let response = rust::pubmed_query(term, "json").await?;
+    eprintln!("Status: {}, URL: {}", response.status(), response.url());
+    eprintln!("Headers:\n{:#?}", response.headers());
 
     let body = response.text().await?;
-    println!("Body: {}", body);
+    println!("{}", body);
 
     Ok(())
 }
