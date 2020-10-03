cf16e82ecc2b985a23216f3165a8bb448d3ff725
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2020-10-03 16:06:36 -0400

    Add pubmed fetch
    and some sample data

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,6 @@
 mod types;
 use anyhow::Error;
 /// term -> response
-/// maybe just need webenv?
-/// ie: https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&query_key=1&usehistory=y&WebEnv=MCID_5f77bdfcb48029616024b3fb&retmode=xml&retmax=100&retstart=18
 /// response -> xml
 /// xml -> id's
 /// ids -> responses
@@ -21,6 +19,34 @@ pub async fn pubmed_search(query: &str) -> Result<Response, Error> {
         ("retmode", "json"),
     ];
     let base_url = BASE_URL.to_string() + "esearch.fcgi";
+
+    client
+        .get(&base_url)
+        .query(&query)
+        .send()
+        .await
+        .map_err(|e| e.into())
+}
+
+/// maybe just need webenv?
+/// ie: https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&query_key=1&usehistory=y&WebEnv=MCID_5f77bdfcb48029616024b3fb&retmode=xml&retmax=100&retstart=18
+pub async fn pubmed_fetch(
+    query_key: usize,
+    web_env: &str,
+    retmax: usize,
+    retstart: usize,
+) -> Result<Response, Error> {
+    let client = Client::new();
+    let query = [
+        ("db", "pubmed"),
+        ("query_key", &query_key.to_string()),
+        ("usehistory", "y"),
+        ("WebEnv", web_env),
+        ("retmode", "xml"),
+        ("retmax", &retmax.to_string()),
+        ("retstart", &retstart.to_string()),
+    ];
+    let base_url = BASE_URL.to_string() + "efetch.fcgi";
 
     client
         .get(&base_url)
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,7 +7,10 @@ async fn main() -> Result<(), Error> {
     eprintln!("Status: {}, URL: {}", response.status(), response.url());
     eprintln!("Headers:\n{:#?}", response.headers());
 
-    let body = response.text().await?;
+    let response = rust::search_response_to_result(response).await?;
+
+    let fetch_response = rust::pubmed_fetch(response.querykey, &response.webenv, 100, 0).await?;
+    let body = fetch_response.text().await?;
     println!("{}", body);
 
     Ok(())
